28d8fdda684d3a016ba3877970d9cfe91457cee8
WTobor <wero15@op.pl>
2020-05-13 15:45:08 +0200

    added Database project with inMemory database configuration

---
+++ MyNozbe.Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MyNozbe.Database.Models;
+
+namespace MyNozbe.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+    }
+}
---
+++ MyNozbe.Database/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace MyNozbe.Database.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
