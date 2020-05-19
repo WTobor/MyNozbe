fb7ecf14dce8d992254625b239b8456b2690e690
WTobor <wero15@op.pl>
2020-05-19 11:53:29 +0200

    TDD - added E2E tests for Rename and AssignTask

--- MyNozbe.API.E2ETests/ProjectControllerTest.cs
+++ MyNozbe.API.E2ETests/ProjectControllerTest.cs
@@ -74,9 +74,59 @@ namespace MyNozbe.API.E2ETests
             projectResult.Id.Should().Be(projectId);
         }
 
+        [Theory]
+        [AutoData]
+        public async Task Rename_ShouldChangeNameToNewOneAsync(string projectName, string newProjectName)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var projectId = await AddTestProjectAsync(projectName, client);
+            var url = $"project/{projectId}/rename/{newProjectName}";
+
+            // Act
+            var response = await client.PostAsync(url, null);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var projectResponse = await client.GetAsync($"project/{projectId}");
+
+            var projectResult = await ResponseHelper.GetResult<ProjectTestModel>(projectResponse);
+            projectResult.Id.Should().Be(projectId);
+            projectResult.Name.Should().Be(newProjectName);
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task AssignTask_ShouldAssignTaskToProjectAsync(string projectName, string taskName)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var taskId = await AddTestTaskAsync(taskName, client);
+            var projectId = await AddTestProjectAsync(projectName, client);
+            var url = $"project/{projectId}/assignTask/{taskId}";
+
+            // Act
+            var response = await client.PostAsync(url, null);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var projectResponse = await client.GetAsync($"project/{projectId}");
+
+            var projectResult = await ResponseHelper.GetResult<ProjectTestModel>(projectResponse);
+            projectResult.TaskTestModels.Should().Contain(x => x.Id == taskId);
+        }
+
+
         private static async Task<int> AddTestProjectAsync(string name, HttpClient client)
         {
             var url = $"project?name={name}";
+            var response = await client.PostAsync(url, null);
+            return await ResponseHelper.GetResult<int>(response);
+        }
+
+        private static async Task<int> AddTestTaskAsync(string name, HttpClient client)
+        {
+            var url = $"task?name={name}";
             var response = await client.PostAsync(url, null);
             return await ResponseHelper.GetResult<int>(response);
         }
