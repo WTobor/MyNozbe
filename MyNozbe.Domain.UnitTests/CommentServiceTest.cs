e138f1625421df592f92599a20a8fe38d5e0a90a
WTobor <wero15@op.pl>
2020-05-23 18:26:17 +0200

    added missing unit test for CommentService

---
+++ MyNozbe.Domain.UnitTests/CommentServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using Moq;
+using MyNozbe.Domain.Interfaces;
+using MyNozbe.Domain.Models;
+using MyNozbe.Domain.Services;
+using Xunit;
+
+namespace MyNozbe.Domain.UnitTests
+{
+    public class CommentServiceTest
+    {
+        [Theory]
+        [AutoMoqData]
+        public async Task AddComment_ShouldCallAddMethodAsync(
+            [Frozen] Mock<IDbOperations<CommentModel>> commentModelDbOperationsMock,
+            CommentService commentService)
+        {
+            var commentResult = await commentService.AddCommentAsync(1, "testContent");
+
+            commentModelDbOperationsMock.Verify(
+                x => x.AddAsync(It.Is<CommentModel>(y =>
+                    y.TaskId == 1 && y.Content =="testContent")),
+                Times.Once);
+        }
+    }
+}
